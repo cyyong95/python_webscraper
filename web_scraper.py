68289f501bf6c1f2189c9831d2be86accc4fa493
Chun Yu <cyyong95x@gmail.com>
2019-05-19 01:19:52 +0800

    Added gitignore

--- web_scraper.py
+++ web_scraper.py
@@ -1,23 +1,9 @@
 import requests
-from bs4 import BeautifulSoup
-import os
 
 
-def getHTML():
-
-    # Check if user input instagram url
-    # if len(sys.argv) != 1:
-    #     print("Please input a public Instagram profile!")
-
-    url = requests.get("https://www.instagram.com/taylorswift/?hl=en")
-    soup = BeautifulSoup(url.text, 'html.parser')
-    f = open("html.txt", "w+")
-    f.write(url.text)
-    f.close()
-    print(soup.title.string[:-31])
-
-
+def get_request():
+    req = requests.get("")
 
 
 if __name__ == '__main__':
-    getHTML()
+    get_request()
