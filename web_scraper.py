5d9e6d2bfb9e7b701eda4af9ccd120ff5a60e6ab
Chun Yu <cyyong95x@gmail.com>
2019-05-20 15:02:19 +0800

    Changed direction. Not scraping Instagram anymore

--- web_scraper.py
+++ web_scraper.py
@@ -1,8 +1,12 @@
 import requests
+from bs4 import BeautifulSoup
 
 
 def get_request():
-    req = requests.get("")
+    req = requests.get('https://www.yelp.com/search?find_desc=&find_loc=KL')
+    req_soup = BeautifulSoup(req.text, 'html.parser')
+    for name in req_soup.findAll('h3', {'class': 'lemon--h3__373c0__sQmiG'}):
+        print(name.text)
 
 
 if __name__ == '__main__':
