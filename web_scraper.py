b857ab6cc32350c593ff2aeb6b967c55010d5ae8
Chun Yu <cyyong95x@gmail.com>
2019-05-27 00:48:04 +0800

    Added user_agent options

--- web_scraper.py
+++ web_scraper.py
@@ -1,60 +1,77 @@
 import time
 import re
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
 
 
 def launch_chrome():
     # Any url with the wrong search parameter will redirect you to a new search page
     url = "https://www.expedia.com.my/Flights-Search?flight-type=on&starDate=25%2F05%2F2019&endDate=25%2F05%2F20\
-           19&mode=search&trip=roundtrip&leg1=from%3AKuala+Lumpur%2C+Malaysia+%28KUL-Kuala+Lumpur+Intl.%29%2Cto%3\
-           ASingapore%2C+Singapore+%28SIN-Changi%29%2Cdeparture%3A25%2F05%2F2019TANYT&leg2=from%3ASingapore%2C+Si\
-           ngapore+%28SIN-Changi%29%2Cto%3AKuala+Lumpur%2C+Malaysia+%28KUL-Kuala+Lumpur+Intl.%29%2Cdeparture%3A25\
-           %2F05%2F2019TANYT&passengers=children%3A0%2Cadults%3A1%2Cseniors%3A0%2Cinfantinlap%3AY"
+          19&mode=search&trip=roundtrip&leg1=from%3AKuala+Lumpur%2C+Malaysia+%28KUL-Kuala+Lumpur+Intl.%29%2Cto%3\
+          ASingapore%2C+Singapore+%28SIN-Changi%29%2Cdeparture%3A25%2F05%2F2019TANYT&leg2=from%3ASingapore%2C+Si\
+          ngapore+%28SIN-Changi%29%2Cto%3AKuala+Lumpur%2C+Malaysia+%28KUL-Kuala+Lumpur+Intl.%29%2Cdeparture%3A25\
+          %2F05%2F2019TANYT&passengers=children%3A0%2Cadults%3A1%2Cseniors%3A0%2Cinfantinlap%3AY"
+
+    # Add user agent
+    options = Options()
+    ua = UserAgent()
+    user_agent = ua.random
+    options.add_argument(f'user-agent={user_agent}')
 
     # Run the web driver
-    driver = webdriver.Chrome("C:\\Users\\Chun Yu\\Downloads\\chromedriver.exe")
+    driver = webdriver.Chrome(executable_path="C:\\Users\\Chun Yu\\Downloads\\chromedriver.exe", options=options)
     driver.get(url)
     return driver
 
 
 def get_search_page(chrome):
+
+    depart_date = r'14/06/2019'
+    return_date = r'15/06/2019'
+
+    # Let the page load
+    time.sleep(2)
+
     # Input departure location
     departure_input = chrome.find_element_by_id('Leg1departureReenterText')
     departure_input.clear()
     departure_input.send_keys("Kuala Lumpur, Malaysia (KUL-Kuala Lumpur Intl.)")
-    time.sleep(2)
+    time.sleep(1)
 
     # Input arrival location
     arrival_input = chrome.find_element_by_id("Leg1arrivalReenterText")
     arrival_input.clear()
     arrival_input.send_keys("Singapore, Singapore (SIN-All Airports)")
-    time.sleep(2)
+    time.sleep(1)
 
     # Input departure date
     depart_date_input = chrome.find_element_by_id("departDateInput1")
     depart_date_input.clear()
-    depart_date_input.send_keys("26/05/2019")
-    time.sleep(2)
+    depart_date_input.send_keys(f'{depart_date}')
+    time.sleep(1)
 
     # Input arrival date
     return_date_input = chrome.find_element_by_id("returnDateInput1")
     return_date_input.clear()
-    return_date_input.send_keys("26/05/2019")
-    time.sleep(2)
+    return_date_input.send_keys(f'{return_date}')
+    time.sleep(1)
 
     # Click on somewhere to move focus away from DatePicker
     footer_click = chrome.find_element_by_id("footer-copyright-msg")
     footer_click.click()
-    time.sleep(2)
+    time.sleep(1)
 
     # Search for result
     button_search = chrome.find_element_by_id("continue-search")
     button_search.click()
-    time.sleep(2)
 
 
 def get_html(chrome):
+
+    # Let the page load the first time
+    time.sleep(5)
 
     airline_name = []
     airline_price = []
@@ -101,13 +118,8 @@ def get_price(dirty_text):
 
 if __name__ == '__main__':
     chrome_driver = launch_chrome()
-    time.sleep(2)
     get_search_page(chrome_driver)
     get_html(chrome_driver)
-
-
-
-
 
 
 
